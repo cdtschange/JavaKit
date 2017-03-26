f9a7eb4829919c47c5b7e7ff87d3216e30b2ac44
cdts <cdts_change@aliyun.com>
2017-03-26 22:26:10 +0800

    list feature

--- SDK/src/com/javakit/data/convert/XmlConverter.java
+++ SDK/src/com/javakit/data/convert/XmlConverter.java
@@ -1,17 +1,43 @@
 package com.javakit.data.convert;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.json.JSONObject;
+import org.json.XML;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.StringReader;
+import java.io.StringWriter;
 
 /**
  * Created by cdts on 17/03/2017.
  */
 public class XmlConverter {
 
+    public static ObjectMapper objectMapper = new ObjectMapper();
+    public static XmlMapper xmlMapper = new XmlMapper();
+
+    public static JSONObject xml2json(String xml){
+        try {
+            JSONObject jObject = XML.toJSONObject(xml);
+            return jObject;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static String json2xml(String jsonStr)throws Exception{
+        JsonNode root = objectMapper.readTree(jsonStr);
+        String xml = xmlMapper.writeValueAsString(root);
+        return xml;
+    }
 //    public static Document string2xml(String xmlString) {
 //        try {
 //            StringReader sr = new StringReader(xmlString);
@@ -26,7 +52,7 @@ public class XmlConverter {
 //        }
 //    }
 //    /**
-//     * 将xml字符串<STRONG>转换</STRONG>为JSON字符串
+//     * 将xml字符串转换为JSON字符串
 //     *
 //     * @param xmlString
 //     *            xml字符串

7a21ee64d36c332963b95a26cbdef9a2e9118282
wei.mao <wei.mao@jimubox.com>
2017-03-27 14:10:25 +0800

    detail & download

--- SDK/src/com/javakit/dao/BaseKitDAO.java
+++ SDK/src/com/javakit/dao/BaseKitDAO.java
@@ -1,7 +1,57 @@
 package com.javakit.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+
 /**
  * Created by cdts on 22/03/2017.
  */
-public class BaseKitDAO {
+public abstract class BaseKitDAO<T> {
+
+    protected String getIdKey() {
+        return "id";
+    }
+
+    @Autowired
+    protected SessionFactory sessionFactory;
+
+    public T create(T entity) {
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.save(entity);
+            return entity;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public T loadById(String entityId) {
+        return query(getIdKey(), entityId);
+    }
+
+
+    protected T query(String key, String value) {
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        Class <T> entityClass = (Class <T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+        CriteriaQuery<T> cq = builder.createQuery(entityClass);
+        Root<T> root = cq.from(entityClass);
+        Predicate condition = builder.equal(root.get(key), value);
+        cq.where(condition);
+        List<T> list = session.createQuery(cq).getResultList();
+        if (list != null && list.size() > 0) {
+            return list.get(0);
+        } else {
+            return null;
+        }
+    }
 }
--- SDK/src/com/javakit/service/BaseKitService.java
+++ SDK/src/com/javakit/service/BaseKitService.java
@@ -1,7 +1,16 @@
 package com.javakit.service;
 
+import com.javakit.dao.BaseKitDAO;
+
 /**
  * Created by cdts on 22/03/2017.
  */
-public class BaseKitService {
+public abstract class BaseKitService<T> {
+
+    public abstract BaseKitDAO<T> getDAO();
+
+    public T loadById(String entityId) {
+        return getDAO().loadById(entityId);
+    }
+
 }

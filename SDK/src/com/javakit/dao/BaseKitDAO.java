96fc32afe9db69f970fac8bfbf3b9be99a2c756a
wei.mao <wei.mao@jimubox.com>
2017-03-28 11:35:53 +0800

    add method

--- SDK/src/com/javakit/dao/BaseKitDAO.java
+++ SDK/src/com/javakit/dao/BaseKitDAO.java
@@ -10,7 +10,10 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by cdts on 22/03/2017.
@@ -34,24 +37,58 @@ public abstract class BaseKitDAO<T> {
         }
     }
 
+    public T update(T entity) {
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.update(entity);
+            return entity;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
     public T loadById(String entityId) {
         return query(getIdKey(), entityId);
     }
 
 
+    public List<T> loadAll() {
+        return queryList(null);
+    }
+
+
     protected T query(String key, String value) {
+        return query(new HashMap<String, String>() {{
+            put(key, value);
+        }});
+    }
+    protected T query(Map<String, String> map) {
+        List<T> list = queryList(map);
+        if (list != null && list.size() > 0) {
+            return list.get(0);
+        } else {
+            return null;
+        }
+    }
+    protected List<T> queryList(Map<String, String> map) {
         Session session = sessionFactory.getCurrentSession();
         CriteriaBuilder builder = session.getCriteriaBuilder();
         Class <T> entityClass = (Class <T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
         CriteriaQuery<T> cq = builder.createQuery(entityClass);
         Root<T> root = cq.from(entityClass);
-        Predicate condition = builder.equal(root.get(key), value);
-        cq.where(condition);
+        if (map != null && map.size() > 0) {
+            List<Predicate> conditions = new ArrayList<>();
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                Predicate condition = builder.equal(root.get(entry.getKey()), entry.getValue());
+                conditions.add(condition);
+            }
+            cq.where(conditions.toArray(new Predicate[0]));
+        }
         List<T> list = session.createQuery(cq).getResultList();
         if (list != null && list.size() > 0) {
-            return list.get(0);
+            return list;
         } else {
-            return null;
+            return new ArrayList<>();
         }
     }
 }
--- SDK/src/com/javakit/service/BaseKitService.java
+++ SDK/src/com/javakit/service/BaseKitService.java
@@ -2,6 +2,8 @@ package com.javakit.service;
 
 import com.javakit.dao.BaseKitDAO;
 
+import java.util.List;
+
 /**
  * Created by cdts on 22/03/2017.
  */
@@ -11,6 +13,9 @@ public abstract class BaseKitService<T> {
 
     public T loadById(String entityId) {
         return getDAO().loadById(entityId);
+    }
+    public List<T> loadAll() {
+        return getDAO().loadAll();
     }
 
 }

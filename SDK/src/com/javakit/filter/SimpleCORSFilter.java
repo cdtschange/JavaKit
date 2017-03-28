5e032a2b6ac38cebd74620c323fd3ae3443d51ea
wei.mao <wei.mao@jimubox.com>
2017-03-28 17:02:20 +0800

    add filter

---
+++ SDK/src/com/javakit/filter/SimpleCORSFilter.java
@@ -0,0 +1,24 @@
+package com.javakit.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by cdts on 28/03/2017.
+ */
+@Component
+public class SimpleCORSFilter implements Filter {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(req, res);
+    }
+    public void init(FilterConfig filterConfig) {}
+    public void destroy() {}
+}
